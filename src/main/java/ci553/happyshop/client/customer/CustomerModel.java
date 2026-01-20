eebbe9075c36732e819ac08d14a9428a48342665
Harry-parker9 <h.parker9@uni.brighton.ac.uk>
2026-01-20 02:18:11 +0000

    updated groupProductsById as the orderQuantity was always set to 1 so when trying to buy more stock than available, the order would go through anyway. Fixed this by making a copy

--- src/main/java/ci553/happyshop/client/customer/CustomerModel.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerModel.java
@@ -181,9 +181,11 @@ public class CustomerModel {
                 Product existing = grouped.get(id);
                 existing.setOrderedQuantity(existing.getOrderedQuantity() + p.getOrderedQuantity());
             } else {
-                // Make a shallow copy to avoid modifying the original
-                grouped.put(id,new Product(p.getProductId(),p.getProductDescription(),
-                        p.getProductImageName(),p.getUnitPrice(),p.getStockQuantity()));
+                // Make a copy to avoid modifying the original
+                Product copy = new Product(p.getProductId(),p.getProductDescription(),
+                        p.getProductImageName(),p.getUnitPrice(),p.getStockQuantity());
+                copy.setOrderedQuantity(p.getOrderedQuantity());
+                grouped.put(id, copy);
             }
         }
         return new ArrayList<>(grouped.values());
